4818723ebe8a86c4b7dd4b22dae0fa9545dca103
John Furtado <jf8846@gmail.com>
2017-12-19 19:07:41 -0500

    Understanding keywords and expressions

---
+++ src/com/Madpoints/Main.java
@@ -0,0 +1,15 @@
+package com.Madpoints;
+
+public class Main {
+
+    public static void main(String[] args) {
+    //dataType/expression
+        double km = (100 * 1.689344);
+        int highScore = 50;
+            // expression
+        if (highScore == 50) {
+                                // expression
+            System.out.println("This is an expression");
+        }
+    }
+}
